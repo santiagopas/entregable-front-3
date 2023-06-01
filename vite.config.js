8d413f7c3b0e57c41cb44d731d24f64b8fdbf295
santiago pasciullo <santiagopasciullo@gmail.com>
2023-05-31 22:59:24 -0300

    :arrow_up: Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/santiagopas/entregable-front-3'
+  base: 'entregable-front-3'
 })
